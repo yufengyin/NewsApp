00a11fdfabb1b8e9cd095e3401c343ac3718bdd5
yyf <321yyf@sina.cn>
2017-09-05 19:38:43 +0800

    add interest view

--- app/src/main/java/com/thirty/java/newsapp/MainActivity.java
+++ app/src/main/java/com/thirty/java/newsapp/MainActivity.java
@@ -9,14 +9,17 @@ import android.view.View;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView mRecyclerView, mInterestView;
+    private RecyclerView.Adapter mAdapter, mInterestAdapter;
+    private RecyclerView.LayoutManager mLayoutManager, mInterestLayoutManager;
     private Button mCollectButton, mSetButton;
     private News[] myDataset = new News[]{
             new News("fsy","tai qiang la"), new News("yyf","tai ruo la"),
             new News("fsy","tai qiang la"), new News("yyf","tai ruo la"),
             new News("fsy","tai qiang la"), new News("yyf","tai ruo la")
+    };
+    private String[] myInterestDataset = new String[]{
+            "推荐", "科技", "教育", "军事", "国内", "社会", "文化"
     };
 
     @Override
@@ -24,18 +27,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_index_view);
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
+        mInterestView = (RecyclerView) findViewById(R.id.my_interest_view);
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
+        mInterestView.setHasFixedSize(true);
 
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
+        mInterestLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mInterestView.setLayoutManager(mInterestLayoutManager);
 
         // specify an adapter (see also next example)
         mAdapter = new MyAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
+        mInterestAdapter = new MyInterestAdapter(myInterestDataset);
+        mInterestView.setAdapter(mInterestAdapter);
 
         //我的收藏切换
         mCollectButton = (Button) findViewById(R.id.collect_button);
