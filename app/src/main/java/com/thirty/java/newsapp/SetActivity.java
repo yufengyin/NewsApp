a03eff051ce45963db895b439b6547daa717290c
yyf <321yyf@sina.cn>
2017-09-05 21:42:04 +0800

    modify menu

--- app/src/main/java/com/thirty/java/newsapp/SetActivity.java
+++ app/src/main/java/com/thirty/java/newsapp/SetActivity.java
@@ -1,10 +1,16 @@
 package com.thirty.java.newsapp;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.view.View;
+import android.widget.Toast;
 
 
 /**
@@ -22,7 +28,7 @@ public class SetActivity extends AppCompatActivity {
         mIndexButton = (Button) findViewById(R.id.index_button);
         mIndexButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 finish();
             }
         });
@@ -31,7 +37,7 @@ public class SetActivity extends AppCompatActivity {
         mCollectButton = (Button) findViewById(R.id.collect_button);
         mCollectButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(SetActivity.this, CollectActivity.class);
                 startActivity(intent);
                 finish();
