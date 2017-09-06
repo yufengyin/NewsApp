27e2c33b1a06a04d85e934dbb1ce94592417cfda
zzzyj <31604407+zzzyj@users.noreply.github.com>
2017-09-06 09:59:45 +0800

    Please enter the commit message for your changes. Lines starting

---
+++ app/src/main/java/com/thirty/java/newsapp/NewsRunnable.java
@@ -0,0 +1,37 @@
+package com.thirty.java.newsapp;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+/**
+ * Created by zyj on 2017/9/5.
+ */
+
+class NewsRunnable implements Runnable
+{
+    NewsRunnable(Handler handler, int pageNo, int pageSize)
+    {
+        this.handler = handler;
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+    }
+    @Override
+    public void run()
+    {
+        try
+        {
+            Bundle bundle = NewsApiCaller.getLatestNews(pageNo, pageSize);
+            Message message = Message.obtain();
+            message.setData(bundle);
+            handler.sendMessage(message);
+        }
+        catch (Exception e)
+        { Log.d("back", "failure in latestNewsRunnable()"); }
+    }
+    Handler handler;
+    int pageNo;
+    int pageSize;
+}
+
