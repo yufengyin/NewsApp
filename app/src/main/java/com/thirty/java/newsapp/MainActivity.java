30fe782825544b775d179c9f96599c1e313d125e
fsygd <fsygd1996@163.com>
2017-09-07 13:33:35 +0800

    voice synthesize

--- app/src/main/java/com/thirty/java/newsapp/MainActivity.java
+++ app/src/main/java/com/thirty/java/newsapp/MainActivity.java
@@ -20,6 +20,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import com.iflytek.cloud.*;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView, mInterestView;
@@ -38,10 +39,58 @@ public class MainActivity extends AppCompatActivity {
             "推荐", "科技", "教育", "军事", "国内", "社会", "文化"
     };
 
-    @Override
+    //讯飞语音合成器
+    private SynthesizerListener mSynListener = new SynthesizerListener() {
+        //会话结束回调接口，没有错误时，error为null
+        public void onCompleted(SpeechError error) {
+            Log.i("xunfei", "" + error);
+        }
+        //缓冲进度回调
+        //percent为缓冲进度0~100，beginPos为缓冲音频在文本中开始位置，endPos表示缓冲音频在文本中结束位置，info为附加信息。
+        public void onBufferProgress(int percent, int beginPos, int endPos, String info) {
+            Log.i("xunfei", "onBufferProgress");
+        }
+        //开始播放
+        public void onSpeakBegin() {
+            Log.i("xunfei", "onSpeakBegin");
+        }
+        //暂停播放
+        public void onSpeakPaused() {
+            Log.i("xunfei", "onSpeakPaused");
+        }
+        //播放进度回调
+        //percent为播放进度0~100,beginPos为播放音频在文本中开始位置，endPos表示播放音频在文本中结束位置.
+        public void onSpeakProgress(int percent, int beginPos, int endPos) {
+            Log.i("xunfei", "onSpeakProgress");
+        }
+        //恢复播放回调接口
+        public void onSpeakResumed() {
+            Log.i("xunfei", "onSpeakResumed");
+        }
+        //会话事件回调接口
+        public void onEvent(int arg0, int arg1, int arg2, Bundle arg3) {
+            Log.i("xunfei", "onEvent");
+        }
+    };
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_index_view);
+
+            Log.i("xunfei", "start");
+        //讯飞初始化
+        SpeechUtility.createUtility(this, SpeechConstant.APPID + "=59b0ae8e");
+
+        //语音合成
+        SpeechSynthesizer mTts = SpeechSynthesizer.createSynthesizer(MainActivity.this, null);
+        mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaoyan");//设置发音人
+        mTts.setParameter(SpeechConstant.SPEED, "50");//设置语速
+        mTts.setParameter(SpeechConstant.VOLUME, "80");//设置音量，范围0~100
+        mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD); //设置云端
+
+        mTts.startSpeaking("膜峰膜峰膜峰", mSynListener);
+            Log.i("xunfei", "end");
 
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         //mInterestView = (RecyclerView) findViewById(R.id.my_interest_view);
